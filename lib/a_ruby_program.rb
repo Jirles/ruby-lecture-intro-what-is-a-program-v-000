341d3162bb2d9bfc00c36fadf89388201b6f0e33
Emily Jirles <emily.jirles@gmail.com>
2018-04-23 16:47:14 +0000

    automatically backed up by learn

--- lib/a_ruby_program.rb
+++ lib/a_ruby_program.rb
@@ -20,4 +20,4 @@ puts "Finally, barewords, or variables, that are set equal to things."
 
 puts "Anything that isn't one of those will cause an error."
 #there wasn't really anything to do with this lab. So I removed the error
-puts "see."
+puts "see"
